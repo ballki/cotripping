e4889e95deb62a05df5487b67046013ff5deda4e
ballki <kball369@gmail.com>
2014-06-11 17:19:01 +0300

    Delete omniauth.rb

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '299911060178158', 'ac10f6fa59604c2dc3f979d3ff2f7257'
-end
